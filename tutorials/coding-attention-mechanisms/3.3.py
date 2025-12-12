7966e792970faa1bdc832a3f77a1e5096d99af4d
vrotondo <vrfr91@gmail.com>
2025-12-12 11:18:41 -0500

    Finished Lesson Chapter 3.3

--- tutorials/coding-attention-mechanisms/3.3.py
+++ tutorials/coding-attention-mechanisms/3.3.py
@@ -57,5 +57,52 @@ for idx, element in enumerate(inputs[0]):
 
 attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
 
-print("Attention weights:", attn_weights_2_tmp)
-print("Sum:", attn_weights_2_tmp.sum())
+#print("Attention weights:", attn_weights_2_tmp)
+#print("Sum:", attn_weights_2_tmp.sum())
+
+def softmax_naive(x):
+    return torch.exp(x) / torch.exp(x).sum(dim=0)
+
+attn_weights_2_naive = softmax_naive(attn_scores_2)
+
+#print("Attention weights:", attn_weights_2_naive)
+#print("Sum:", attn_weights_2_naive.sum())
+
+attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
+
+#print("Attention weights:", attn_weights_2)
+#print("Sum:", attn_weights_2.sum())
+
+query = inputs[1] # 2nd input token is the query
+
+context_vec_2 = torch.zeros(query.shape)
+for i, x_i in enumerate(inputs):
+    context_vec_2 += attn_weights_2[i]*x_i
+
+#print(context_vec_2)
+
+# COMPUTING ATTENTION WEIGHTS FOR ALL INPUT TOKENS
+
+attn_scores = torch.empty(6, 6)
+
+for i, x_i in enumerate(inputs):
+    for j, x_j in enumerate(inputs):
+        attn_scores[i, j] = torch.dot(x_i, x_j)
+
+#print(attn_scores)
+
+attn_scores = inputs @ inputs.T
+#print(attn_scores)
+
+attn_weights = torch.softmax(attn_scores, dim=-1)
+#print(attn_weights)
+
+row_2_sum = sum([0.1385, 0.2379, 0.2333, 0.1240, 0.1082, 0.1581])
+#print("Row 2 sum:", row_2_sum)
+
+#print("All row sums:", attn_weights.sum(dim=-1))
+
+all_context_vecs = attn_weights @ inputs
+#print(all_context_vecs)
+
+#print("Previous 2nd context vector:", context_vec_2)
