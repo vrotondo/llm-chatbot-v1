4236b0df3f3bf89d7822f6f4fde5aa1e82301cbe
vrotondo <vrfr91@gmail.com>
2025-12-16 16:00:06 -0500

    Normalization

--- tutorials/implementing-gpt-model-from-scratch-to-generate-text/4.0.py
+++ tutorials/implementing-gpt-model-from-scratch-to-generate-text/4.0.py
@@ -91,3 +91,53 @@ logits = model(batch)
 
 # NORMALIZING ACTIVATIONS WITH LAYER NORMALIZATION
 
+torch.manual_seed(123)
+
+# create 2 training examples with 5 dimensions (features) each
+batch_example = torch.randn(2, 5) 
+
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+#print(out)
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+
+#print("Mean:\n", mean)
+#print("Variance:\n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+#print("Normalized layer outputs:\n", out_norm)
+
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+#print("Mean:\n", mean)
+#print("Variance:\n", var)
+      
+torch.set_printoptions(sci_mode=False)
+#print("Mean:\n", mean)
+#print("Variance:\n", var)
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased =False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+
+#print("Mean:\n", mean)
+#print("Variance:\n", var)
+
+# IMPLEMENTING A FEED FORWARD NETWORK WITH GELU ACTIVATIONS
+
