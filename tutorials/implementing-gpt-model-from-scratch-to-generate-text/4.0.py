9702bb44519f7d722ad2e0ff1028632ee14962d6
vrotondo <vrfr91@gmail.com>
2026-02-10 13:11:48 -0500

    Finished chapter 4 tutorial

--- tutorials/implementing-gpt-model-from-scratch-to-generate-text/4.0.py
+++ tutorials/implementing-gpt-model-from-scratch-to-generate-text/4.0.py
@@ -358,3 +358,56 @@ total_size_mb = total_size_bytes / (1024 * 1024)
 
 print(f"Total size of the model: {total_size_mb:.2f} MB")'''
 
+# GENERATING TEXT #
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+        
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+        
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        # Focus only on the last time step
+        # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]  
+
+        # Apply softmax to get probabilities
+        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+
+        # Get the idx of the vocab entry with the highest probability value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
+
+start_context = "Hello, I am"
+
+encoded = tokenizer.encode(start_context)
+#print("encoded:", encoded)
+
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+#print("encoded_tensor.shape:", encoded_tensor.shape)
+
+model.eval() # disable dropout
+
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor, 
+    max_new_tokens=6, 
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+
+#print("Output:", out)
+#print("Output length:", len(out[0]))
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+#print(decoded_text)
+
